f2e1dde23b5b5ae2cf99a73d04469b13ffa9a60b
MinJoon_Choi <minjoon1998@gmail.com>
2024-02-07 15:48:52 +0900

    feat: Add files(js, css)

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,10 +1,16 @@
 import HandIcon from './HandIcon';
+import './HandButton.css';
 
+// CSS 파일로 스타일을 적용해 주세요
 function HandButton({ value, onClick }) {
-  const handleClick = () => onClick(value);
+  const handleClick = () => {
+    onClick(value);
+  };
+
   return (
-    <button type="button" onClick={handleClick} aria-label="Like Button">
-      <HandIcon value={value} />
+    // eslint-disable-next-line
+    <button type="button" onClick={handleClick} className="HandButton">
+      <HandIcon value={value} className="HandButton-icon" />
     </button>
   );
 }
